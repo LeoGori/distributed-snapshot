70f273c53b0380048cf50d8ec6e8293991b6f081
LeoGori <leonardogori@hotmail.it>
2023-09-13 16:16:21 +0200

    Fixed sate update synchronized

--- src/Node.java
+++ src/Node.java
@@ -41,7 +41,7 @@ public class Node extends Neighbor {
 
         for (NetworkInterface netint : Collections.list(interfaces)) {
             displayInterfaceInformation(netint);
-            if (netint.getName().equals("enp0se")) {
+            if (netint.getName().equals("enp0s3")) {
                 Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
                 for (InetAddress inetAddress : Collections.list(inetAddresses)) {
                     if (inetAddress instanceof Inet4Address) {
